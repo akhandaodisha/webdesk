4c520e4470d01398a7637a0cb8ec4c555caea263
Amresh <amreshtech@gmail.com>
2018-07-26 20:42:32 +0530

    Auto-generated commit

--- src/app/blog/blog-truncate.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { BlogTruncatePipe } from './blog-truncate.pipe';
-
-describe('BlogTruncatePipe', () => {
-  it('create an instance', () => {
-    const pipe = new BlogTruncatePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
